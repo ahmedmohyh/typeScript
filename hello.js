c0b670fe2e9c1f3269ddbd77e3669596957a154f
Ahmed <amohy2015@gmail.com>
2022-03-26 11:59:38 +0100

    finishing type script

--- hello.js
+++ hello.js
@@ -1,13 +1,58 @@
-var Greeting = /** @class */ (function () {
-    function Greeting(name, message) {
+"use strict";
+/* function age(constructor:Function){
+    console.log('in decorator function',constructor);
+    constructor.prototype.age = 23;
+}
+
+@age
+class Greeting{
+    name:string;
+    message:string;
+    constructor(name:string,message:string){
         this.name = name;
         this.message = message;
     }
-    Greeting.prototype.sayHello = function () {
-        console.log("".concat(this.name, " ").concat(this.message));
+    sayHello(){
+        console.log(`${this.message} ${this.name} Your Age is: ${this.age}`);
+    }
+}
+console.log(new Greeting('Ahmed','Hi').sayHello());
+ */
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+/* function classDecrator<T extends{new (...args: any[]):{}}
+>(constructor:T){
+    return class extends constructor{
+        new_property= 'new Property';
+        hello="override";
     };
-    return Greeting;
-}());
-console.log(new Greeting('Ahmed', 'Hi Ahmed'));
-console.log();
-console.log();
+
+}
+
+@classDecrator
+class Greeting {
+    property="property";
+    hello:string;
+    constructor(m:string){
+        this.hello = m;
+    }
+}
+console.log(new Greeting('Hi')); */
+function student(config) {
+    return function (target) {
+        console.log(config, target);
+        Object.defineProperty(target.prototype, 'course', { value: () => config.course });
+    };
+}
+let person = class person {
+};
+person = __decorate([
+    student({
+        course: 'angular 9'
+    })
+], person);
+console.log(new person().course());
--- hello.ts
+++ hello.ts
@@ -1,3 +1,9 @@
+/* function age(constructor:Function){
+    console.log('in decorator function',constructor);  
+    constructor.prototype.age = 23;
+}
+
+@age
 class Greeting{
     name:string;
     message:string;
@@ -6,9 +12,40 @@ class Greeting{
         this.message = message;
     }
     sayHello(){
-        console.log(`${this.name} ${this.message}`);
+        console.log(`${this.message} ${this.name} Your Age is: ${this.age}`);
     }
 }
-console.log(new Greeting('Ahmed','Hi Ahmed'));
-console.log();
-console.log();
+console.log(new Greeting('Ahmed','Hi').sayHello());
+ */
+
+/* function classDecrator<T extends{new (...args: any[]):{}} 
+>(constructor:T){
+    return class extends constructor{
+        new_property= 'new Property';
+        hello="override";
+    };
+
+}
+
+@classDecrator
+class Greeting {
+    property="property";
+    hello:string;
+    constructor(m:string){
+        this.hello = m;
+    }
+}
+console.log(new Greeting('Hi')); */
+function student(config:any){
+    return function(target:any){
+        console.log(config,target);
+        Object.defineProperty(target.prototype,'course',{value:
+            ()=> config.course});
+    };
+}
+@student({
+    course:'angular 9'
+})
+class person{}
+
+console.log(new person().course());
